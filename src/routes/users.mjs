60e7323256627a2a4152ecbe7adce7634306bec0
Andrew5-Dev <topchiy.ap@gmail.com>
2024-09-04 16:48:06 +0300

    first commit

---
+++ src/routes/users.mjs
@@ -0,0 +1,22 @@
+import {Router} from 'express'
+import {
+    getUsersHandler,
+    postUsersHandler,
+    getUserByIdHandler,
+    putUserByIdHandler,
+    deleteUserByIdHandler
+} from '../controllers/users.mjs'
+
+import {validateParamsUserId, validateUserPost, validateUserPut} from '../validators/userValidator.mjs'
+
+const usersRouter = Router()
+
+usersRouter.route('/').get(getUsersHandler).post(validateUserPost, postUsersHandler)
+
+usersRouter
+    .route('/:userId')
+    .get(validateParamsUserId, getUserByIdHandler)
+    .put(validateParamsUserId, validateUserPut, putUserByIdHandler)
+    .delete(validateParamsUserId, deleteUserByIdHandler)
+
+export default usersRouter
